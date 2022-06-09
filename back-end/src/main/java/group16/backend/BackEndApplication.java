9a262e43a997408c845ec0d6caebf5a2fd6e58b5
guanchen <gn942951@dal.ca>
2022-06-09 16:31:05 -0300

    updated the dependencies to connect database better

--- back-end/src/main/java/group16/backend/BackEndApplication.java
+++ back-end/src/main/java/group16/backend/BackEndApplication.java
@@ -1,4 +1,4 @@
-package groupproject.backend;
+package group16.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- back-end/src/test/java/group16/backend/BackEndApplicationTests.java
+++ back-end/src/test/java/group16/backend/BackEndApplicationTests.java
@@ -1,4 +1,4 @@
-package groupproject.backend;
+package group16.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
