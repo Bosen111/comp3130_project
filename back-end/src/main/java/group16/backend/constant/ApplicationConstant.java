29d2e01ea544e833d0dfbfa71d59652bfbc04fa5
guanchen <gn942951@dal.ca>
2022-07-22 18:12:30 -0300

    Finished the smells analyze and refactoring.

--- back-end/src/main/java/group16/backend/constant/ApplicationConstant.java
+++
@@ -1,9 +0,0 @@
-package group16.backend.constant;
-
-public class ApplicationConstant {
-
-    public static final String ID = "id";
-    public static final String STATUS = "status";
-    public static final String MESSAGE = "message";
-    
-}
